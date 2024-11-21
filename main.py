aac1e3630f979c64e0643e0d83b619450a5c5299
CEA2520 <abram.cheyenne@outlook.com>
2024-11-20 22:23:58 -0800

    added the player triangle to the various .py pages

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
 
@@ -14,14 +15,18 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-         # limit the framerate to 60 FPS
+        
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
