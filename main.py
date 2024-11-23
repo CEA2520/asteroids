d71bf9572de8f7a5e772ccbfcb911da6cf8654a7
CEA2520 <abram.cheyenne@outlook.com>
2024-11-23 12:56:29 -0800

    added .split() for asteroid size and velocity changes

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def main():
         for asteroid in asteroids:
             for shot in shots:
                 if asteroid.collides(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
         screen.fill("black")
